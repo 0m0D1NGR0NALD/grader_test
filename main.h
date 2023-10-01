a915a8dd800f1cbed5bb88409ec9259a2f64ac65
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-10-01 10:49:37 +0300

    Update main.h

--- main.h
+++ main.h
@@ -7,24 +7,25 @@
 
 //using namespace std;
 
-int count_digits(double num){
+int add_digits(double num1, double num2){
    // put your code here
+   return num1 + num2
 }
 
 
-int extract_decimal(double num){
-    // put your code here
-}
+// int extract_decimal(double num){
+//     // put your code here
+// }
 
-int sum_of_digits(int numb){
-// put your code here
-}
+// int sum_of_digits(int numb){
+// // put your code here
+// }
 
-bool is_valid(double value){
-    // call digits_count here and store output
-    // call extract_decimal here and store output
-    // call sum_of_digits here and store output
+// bool is_valid(double value){
+//     // call digits_count here and store output
+//     // call extract_decimal here and store output
+//     // call sum_of_digits here and store output
 
-    // compare output of sum_of_digits and digits_count here
-}
+//     // compare output of sum_of_digits and digits_count here
+// }
 #endif // MAIN_H
